d19df1f11086482b37181df83cfbd66a42259759
RZCoding07 <muhammadrizkylubis@students.polmed.ac.id>
2024-08-31 14:51:04 +0700

    My Project

--- app/Controllers/StokBibit.php
+++ app/Controllers/StokBibit.php
@@ -26,7 +26,7 @@ class StokBibit extends BaseController
             'title' => $this->title,
             'controller' => $this->controller,
 
-            'thead' => ['No', 'Regional' , 'Kebun', 'Varietas', 'Lokasi', 'Nursery Month 1', 'Nursery Month 2', 'Nursery Month 3', 'Nursery Month 4', 'Nursery Month 5', 'Nursery Month 6', 'Nursery Month 7', 'Nursery Month 8', 'Nursery Month 9', 'Nursery Month 10', 'Nursery Month 11', 'Nursery Month 12', 'Nursery Month 13', 'Nursery Month 14', 'Nursery Month 15', 'Nursery Month 16', 'Nursery Month 17', 'Nursery Month 18', 'Nursery Month 19', 'Nursery Month 20', 'Nursery Month 21', 'Nursery Month 22', 'Nursery Month 23', 'Nursery Month 24', 'Nursery Month 25', 'Nursery Month 26', 'Nursery Month 27', 'Nursery Month 28', 'Nursery Month 29', 'Nursery Month 30 Plus', 'Jumlah'],
+            'thead' => ['No', 'Regional' , 'Kebun', 'Varietas', 'Lokasi', 'Nursery Month 1', 'Nursery Month 2', 'Nursery Month 3', 'Nursery Month 4', 'Nursery Month 5', 'Nursery Month 6', 'Nursery Month 7', 'Nursery Month 8', 'Nursery Month 9', 'Nursery Month 10', 'Nursery Month 11', 'Nursery Month 12', 'Nursery Month 13', 'Nursery Month 14', 'Nursery Month 15', 'Nursery Month 16', 'Nursery Month 17', 'Nursery Month 18', 'Nursery Month 19', 'Nursery Month 20', 'Nursery Month 21', 'Nursery Month 22', 'Nursery Month 23', 'Nursery Month 24', 'Nursery Month 25', 'Nursery Month 26', 'Nursery Month 27', 'Nursery Month 28', 'Nursery Month 29', 'Nursery Month 30 Plus', 'Jumlah', 'Bulan', 'Tahun'],
             'tabel' => $this->getTable(['url' => base_url($this->controller . '/tebel')])
         ];
 
@@ -36,7 +36,7 @@ class StokBibit extends BaseController
 
     public function tebel()
     {
-        $builder = db_connect()->table('stok_lokasi_per_var')->select('regional, kebun, varietas, lokasi, nursery_month_1, nursery_month_2, nursery_month_3, nursery_month_4, nursery_month_5, nursery_month_6, nursery_month_7, nursery_month_8, nursery_month_9, nursery_month_10, nursery_month_11, nursery_month_12, nursery_month_13, nursery_month_14, nursery_month_15, nursery_month_16, nursery_month_17, nursery_month_18, nursery_month_19, nursery_month_20, nursery_month_21, nursery_month_22, nursery_month_23, nursery_month_24, nursery_month_25, nursery_month_26, nursery_month_27, nursery_month_28, nursery_month_29, nursery_month_30_plus, jumlah');
+        $builder = db_connect()->table('stok_lokasi_per_var')->select('regional, kebun, varietas, lokasi, nursery_month_1, nursery_month_2, nursery_month_3, nursery_month_4, nursery_month_5, nursery_month_6, nursery_month_7, nursery_month_8, nursery_month_9, nursery_month_10, nursery_month_11, nursery_month_12, nursery_month_13, nursery_month_14, nursery_month_15, nursery_month_16, nursery_month_17, nursery_month_18, nursery_month_19, nursery_month_20, nursery_month_21, nursery_month_22, nursery_month_23, nursery_month_24, nursery_month_25, nursery_month_26, nursery_month_27, nursery_month_28, nursery_month_29, nursery_month_30_plus, jumlah, bulan, tahun');
         return DataTable::of($builder)->hide('id')->addNumbering()->toJson();
     }
 
@@ -114,7 +114,9 @@ class StokBibit extends BaseController
                 'nursery_month_27' => $data[30],
                 'nursery_month_28' => $data[31],
                 'nursery_month_29' => $data[32],
-                'nursery_month_30_plus' => $data[33]
+                'nursery_month_30_plus' => $data[33],
+                'bulan' => $data[34],
+                'tahun' => $data[35],
             ];
         }
 
--- app/Models/StoklokasipervarModel.php
+++ app/Models/StoklokasipervarModel.php
@@ -8,7 +8,7 @@ class StoklokasipervarModel extends Model {
 	protected $primaryKey = 'id';
 	protected $returnType = 'object';
 	protected $useSoftDeletes = false;
-	protected $allowedFields = ['regional', 'kebun', 'varietas', 'lokasi', 'nursery_month_1', 'nursery_month_2', 'nursery_month_3', 'nursery_month_4', 'nursery_month_5', 'nursery_month_6', 'nursery_month_7', 'nursery_month_8', 'nursery_month_9', 'nursery_month_10', 'nursery_month_11', 'nursery_month_12', 'nursery_month_13', 'nursery_month_14', 'nursery_month_15', 'nursery_month_16', 'nursery_month_17', 'nursery_month_18', 'nursery_month_19', 'nursery_month_20', 'nursery_month_21', 'nursery_month_22', 'nursery_month_23', 'nursery_month_24', 'nursery_month_25', 'nursery_month_26', 'nursery_month_27', 'nursery_month_28', 'nursery_month_29', 'nursery_month_30_plus'];
+	protected $allowedFields = ['regional', 'kebun', 'varietas', 'lokasi', 'nursery_month_1', 'nursery_month_2', 'nursery_month_3', 'nursery_month_4', 'nursery_month_5', 'nursery_month_6', 'nursery_month_7', 'nursery_month_8', 'nursery_month_9', 'nursery_month_10', 'nursery_month_11', 'nursery_month_12', 'nursery_month_13', 'nursery_month_14', 'nursery_month_15', 'nursery_month_16', 'nursery_month_17', 'nursery_month_18', 'nursery_month_19', 'nursery_month_20', 'nursery_month_21', 'nursery_month_22', 'nursery_month_23', 'nursery_month_24', 'nursery_month_25', 'nursery_month_26', 'nursery_month_27', 'nursery_month_28', 'nursery_month_29', 'nursery_month_30_plus, bulan, tahun'];
 	protected $useTimestamps = false;
 	protected $createdField  = 'created_at';
 	protected $updatedField  = 'updated_at';
